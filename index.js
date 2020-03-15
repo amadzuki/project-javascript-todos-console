9625630c854ce3a8c402eea7148aa44db9878886
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-03-15 22:29:28 +0700

    Create searchTodo function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const myTodos = []
+const myTodos = ["Write the letter", "Eat some lunch", "Run around the city"]
 
-const addTodos = () => {
+const addTodo = () => {
   const todo = prompt("Add your task!")
   myTodos.push(todo)
 }
@@ -13,6 +13,23 @@ const showTodos = todos => {
   }
 }
 
-addTodos()
+const searchTodo = (todos, textToSearch) => {
+  const foundTodos = []
+
+  for (index = 0; index < todos.length; index++) {
+    const lowerCaseItem = todos[index].toLowerCase()
+    const lowerCaseText = textToSearch.toLowerCase()
+
+    if (lowerCaseItem.includes(lowerCaseText)) {
+      foundTodos.push(todos[index])
+    }
+  }
+
+  console.log(foundTodos)
+}
+
+addTodo()
 
 showTodos(myTodos)
+
+searchTodo(myTodos, "the")
