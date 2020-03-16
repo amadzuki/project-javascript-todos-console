a7f07e91357cc46bb3a3ac6fa05cea2e6bc402ae
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-03-16 19:12:20 +0700

    Create todo object constructor

--- index.js
+++ index.js
@@ -2,27 +2,48 @@ const myTodos = [
   {
     id: 1,
     task: "Write the letter",
-    tags : ['important']
+    tags: ["important"],
     favorite: true,
     completed: true
   },
   {
     id: 2,
     task: "Eat lunch",
-    tags: ['health']
+    tags: ["health"],
     favorite: false,
     completed: true
   },
   {
     id: 3,
     task: "Run around the City",
-    tags: ['health']
+    tags: ["health"],
     favorite: true,
     completed: false
-  },]
+  }
+]
+// todo object constructor
+class todo {
+  constructor(id, task, tags, favorite, completed) {
+    this.id = id
+    this.task = task
+    this.tags = tags
+    this.favorite = favorite
+    this.completed = completed
+  }
+}
 
-const addTodo = () => {
-  const todo = prompt("Add your task!")
+const addTodo = (newTask, newTags, newFavorite, newCompleted) => {
+  const lastID = myTodos
+    .map(task => task.id)
+    .reduce((max, currentValue) => Math.max(max, currentValue))
+  const newTodo = new todo(
+    lastID + 1,
+    newTask,
+    newTags,
+    newFavorite,
+    newCompleted
+  )
+  todo.task = prompt("Add your task!")
   myTodos.push(todo)
 }
 
